5a50d5065522f33ab725b5675ead112fcee12c6a
679 <apgettman@gmail.com>
2022-11-02 23:10:25 +0300

    oop4

---
+++ src/transport/Competing.java
@@ -0,0 +1,12 @@
+package transport;
+
+public interface Competing {
+
+    String[] getCompetingCommands = new String[0];
+    String[] getAllCommands = new String[]{"Пит-стоп", "Лучшее время круга",
+            "Максимальная скорость"};
+
+    void сompeting(String command, Object[] competingCommands);
+
+    void doCommand(String command);
+}
